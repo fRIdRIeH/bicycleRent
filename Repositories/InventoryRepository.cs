bcb5a96a464262e05a2cd46b1e7ba79debc5d59f
fRIdRIeH <wladwaliullin@yandex.ru>
2025-02-14 17:37:22 +0400

    base2

---
+++ Repositories/InventoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bicycleRent.Repositories
+{
+    public class InventoryRepository
+    {
+    }
+}

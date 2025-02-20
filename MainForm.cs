83ab6d854e3bd48cfc1efcf1a718ac8eb045c032
fRIdRIeH <wladwaliullin@yandex.ru>
2025-02-20 22:33:28 +0400

    added admin

---
+++ MainForm.cs
@@ -0,0 +1,17 @@
+namespace bicycleRent
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.TopMost = true;
+            this.WindowState = FormWindowState.Maximized;
+            //this.FormBorderStyle = FormBorderStyle.None;
+        }
+    }
+}

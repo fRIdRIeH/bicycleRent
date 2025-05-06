b01622205ca7ef0985db79a93cc868093d1e443a
fRIdRIeH <wladwaliullin@yandex.ru>
2025-05-06 23:32:15 +0400

    Update bicycleRentFQW.sql

--- bicycleRentFQW.sql
+++ bicycleRentFQW.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Май 03 2025 г., 17:16
+-- Время создания: Май 06 2025 г., 22:31
 -- Версия сервера: 10.8.4-MariaDB
 -- Версия PHP: 7.2.34
 
@@ -43,8 +43,9 @@ CREATE TABLE `Client` (
 --
 
 INSERT INTO `Client` (`Client_Id`, `Client_Surname`, `Client_Name`, `Client_Patronymic`, `Client_Telephone`, `Client_Address`, `Client_Features`, `Client_Visit_Count`) VALUES
-(1, 'Ворончихин', 'Николай', 'Батькович', '89999999999', 'с. Люк-Шудья', 'Коля)', 0),
-(2, 'Панин', 'Василий', 'Батькович', '89999999999', 'г. Ижевск', 'Вася)', 0);
+(1, 'Ворончихин', 'Николай', 'Батькович', '89991111111', 'деревня блин', 'Коля)', 0),
+(2, 'Панин', 'Василий', 'Батькович', '89992222222', 'г. Ижевск', 'Вася)', 0),
+(4, 'Макаров', 'Александр', 'Александрович', '89993333333', 'г. Ижевск - г. Москва', 'Санёчек)', 0);
 
 -- --------------------------------------------------------
 
@@ -99,19 +100,23 @@ CREATE TABLE `Inventory` (
   `Inventory_Type_Id` int(11) DEFAULT NULL,
   `Inventory_Number` int(11) DEFAULT NULL,
   `Inventory_Rents_Count` int(11) DEFAULT NULL,
-  `Inventory_Total` int(11) DEFAULT NULL
+  `Inventory_Total` int(11) DEFAULT NULL,
+  `Status` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Свободен',
+  `Filial_Id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Дамп данных таблицы `Inventory`
 --
 
-INSERT INTO `Inventory` (`Inventory_Id`, `Inventory_Name`, `Inventory_Type_Id`, `Inventory_Number`, `Inventory_Rents_Count`, `Inventory_Total`) VALUES
-(1, 'Forward', 1, 1, 0, 0),
-(2, 'Forward', 1, 2, 0, 0),
-(3, 'Stels', 1, 3, 0, 0),
-(4, 'Stels', 1, 4, 0, 0),
-(5, 'Gt', 1, 5, 0, 0);
+INSERT INTO `Inventory` (`Inventory_Id`, `Inventory_Name`, `Inventory_Type_Id`, `Inventory_Number`, `Inventory_Rents_Count`, `Inventory_Total`, `Status`, `Filial_Id`) VALUES
+(1, 'Forward', 1, 1, 0, 0, 'В аренде', 1),
+(2, 'Forward', 1, 2, 0, 0, 'Забронирован', 1),
+(3, 'Stels', 1, 3, 0, 0, 'Свободен', 1),
+(4, 'Stels', 1, 4, 0, 0, 'Свободен', 1),
+(5, 'Gt', 1, 5, 0, 0, 'Свободен', 1),
+(6, 'Forward черный', 1, 6, 0, 0, 'Свободен', 1),
+(7, 'Forward черный', 1, 7, 0, 0, 'Свободен', 1);
 
 -- --------------------------------------------------------
 
@@ -132,10 +137,40 @@ CREATE TABLE `Inventory_Price` (
 
 INSERT INTO `Inventory_Price` (`Inventory_Price_Id`, `Price_Id`, `Time_Id`, `Inventory_Id`) VALUES
 (1, 1, 1, 1),
-(2, 2, 2, 2),
-(3, 3, 3, 3),
-(4, 4, 4, 4),
-(5, 5, 5, 5);
+(2, 2, 2, 1),
+(3, 3, 3, 1),
+(4, 4, 4, 1),
+(5, 5, 5, 1),
+(6, 1, 1, 2),
+(7, 2, 2, 2),
+(8, 3, 3, 2),
+(9, 4, 4, 2),
+(10, 5, 5, 2),
+(11, 1, 1, 3),
+(12, 2, 2, 3),
+(13, 3, 3, 3),
+(14, 4, 4, 3),
+(15, 5, 5, 3),
+(16, 1, 1, 4),
+(17, 2, 2, 4),
+(18, 3, 3, 4),
+(19, 4, 4, 4),
+(20, 5, 5, 4),
+(21, 1, 1, 5),
+(22, 2, 2, 5),
+(23, 3, 3, 5),
+(24, 4, 4, 5),
+(25, 5, 5, 5),
+(26, 1, 1, 6),
+(27, 2, 2, 6),
+(28, 3, 3, 6),
+(29, 4, 4, 6),
+(30, 5, 5, 6),
+(31, 1, 1, 7),
+(32, 2, 2, 7),
+(33, 3, 3, 7),
+(34, 4, 4, 7),
+(35, 5, 5, 7);
 
 -- --------------------------------------------------------
 
@@ -192,17 +227,18 @@ CREATE TABLE `Rent` (
   `Total` int(11) DEFAULT NULL,
   `Status` varchar(63) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `User_Id` int(11) DEFAULT NULL,
-  `Deposit_Id` int(11) DEFAULT NULL
+  `Deposit_Id` int(11) DEFAULT NULL,
+  `Created_At` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Дамп данных таблицы `Rent`
 --
 
-INSERT INTO `Rent` (`Rent_Id`, `Filial_Id`, `Client_Id`, `Time_Start`, `Time_End`, `Total`, `Status`, `User_Id`, `Deposit_Id`) VALUES
-(1, 1, 1, '2025-04-29 14:54:38', '2025-04-29 19:54:38', 800, 'Активна', 2, 1),
-(2, 1, 2, '2025-04-29 14:54:38', '2025-04-29 16:54:38', 1200, 'Закрыта', 2, 2),
-(3, 1, 1, '2025-04-29 14:54:38', '2025-04-29 15:54:38', 600, 'Закрыта', 2, 3);
+INSERT INTO `Rent` (`Rent_Id`, `Filial_Id`, `Client_Id`, `Time_Start`, `Time_End`, `Total`, `Status`, `User_Id`, `Deposit_Id`, `Created_At`) VALUES
+(26, 1, 1, '2025-05-06 22:08:29', '2025-05-06 23:08:29', 360, 'В процессе', 1, 1, '2025-05-06 21:09:11'),
+(27, 1, 1, '2025-05-06 22:21:26', '2025-05-06 23:21:26', 300, 'В процессе', 1, 1, '2025-05-06 21:21:36'),
+(28, 1, 1, '2025-05-06 22:57:35', '2025-05-06 23:57:35', 300, 'В процессе', 1, 1, '2025-05-06 21:57:49');
 
 -- --------------------------------------------------------
 
@@ -212,19 +248,18 @@ INSERT INTO `Rent` (`Rent_Id`, `Filial_Id`, `Client_Id`, `Time_Start`, `Time_End
 
 CREATE TABLE `Rent_has_Inventory` (
   `Rent_Rent_Id` int(11) NOT NULL,
-  `Inventory_Inventory_Id` int(11) NOT NULL
+  `Inventory_Inventory_Id` int(11) NOT NULL,
+  `Selected_Price_Id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Дамп данных таблицы `Rent_has_Inventory`
 --
 
-INSERT INTO `Rent_has_Inventory` (`Rent_Rent_Id`, `Inventory_Inventory_Id`) VALUES
-(1, 1),
-(2, 2),
-(3, 2),
-(3, 4),
-(3, 5);
+INSERT INTO `Rent_has_Inventory` (`Rent_Rent_Id`, `Inventory_Inventory_Id`, `Selected_Price_Id`) VALUES
+(26, 4, 18),
+(27, 4, 17),
+(28, 7, 32);
 
 -- --------------------------------------------------------
 
@@ -301,7 +336,8 @@ ALTER TABLE `Filial`
 --
 ALTER TABLE `Inventory`
   ADD PRIMARY KEY (`Inventory_Id`),
-  ADD KEY `Inventory_Type_Id` (`Inventory_Type_Id`);
+  ADD KEY `Inventory_Type_Id` (`Inventory_Type_Id`),
+  ADD KEY `fk_inventory_filial` (`Filial_Id`);
 
 --
 -- Индексы таблицы `Inventory_Price`
@@ -362,7 +398,7 @@ ALTER TABLE `User`
 -- AUTO_INCREMENT для таблицы `Client`
 --
 ALTER TABLE `Client`
-  MODIFY `Client_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `Client_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- AUTO_INCREMENT для таблицы `Deposit`
@@ -380,13 +416,13 @@ ALTER TABLE `Filial`
 -- AUTO_INCREMENT для таблицы `Inventory`
 --
 ALTER TABLE `Inventory`
-  MODIFY `Inventory_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `Inventory_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT для таблицы `Inventory_Price`
 --
 ALTER TABLE `Inventory_Price`
-  MODIFY `Inventory_Price_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `Inventory_Price_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT для таблицы `Inventory_Type`
@@ -404,7 +440,7 @@ ALTER TABLE `Price`
 -- AUTO_INCREMENT для таблицы `Rent`
 --
 ALTER TABLE `Rent`
-  MODIFY `Rent_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `Rent_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT для таблицы `Time`
@@ -426,6 +462,7 @@ ALTER TABLE `User`
 -- Ограничения внешнего ключа таблицы `Inventory`
 --
 ALTER TABLE `Inventory`
+  ADD CONSTRAINT `fk_inventory_filial` FOREIGN KEY (`Filial_Id`) REFERENCES `Filial` (`Filial_Id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `inventory_ibfk_1` FOREIGN KEY (`Inventory_Type_Id`) REFERENCES `Inventory_Type` (`Inventory_Type_Id`);
 
 --
